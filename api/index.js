285717a2a2817cb1008e702d31077d82c7091d5c
Justin915Nsci <jjustinbt@gmail.com>
2020-09-02 22:59:02 -0400

    working filteredJobs

--- api/index.js
+++ api/index.js
@@ -40,13 +40,61 @@ app.get('/jobs', async (req, res) => {
   res.send(mockJobs)
 })
 
+async function scrapeIndeed(option,location){
+  console.log("scrapping indeed");
+  const jobsData = await indeedScrapper.scrapeIndeed(option,location);
+  console.log("indeed scrapping finished");
+  return jobsData;
+
+}
+
+async function filterBySkills(jobsData, skills){
+  var bodyText = "";
+  var foundSkills = [];
+  var jobJson = {};
+  var filteredJobs = [];
+  var count = 1;
+  for(var i = 0; i<jobsData.length;i++){
+    console.log("filtering job " + count);
+    bodyText = jobsData[i].bodyText;
+    foundSkills = [];
+    for(var j = 0; j<skills.length;j++){
+      console.log("Checking if job has " + skills[j]);
+      if(bodyText.indexOf(skills[j])!==-1){
+        foundSkills.push(skills[j]);
+      }
+    }
+    jobJson = {
+      title: jobsData[i].title,
+      company: jobsData[i].company,
+      location: jobsData[i].location,
+      link: jobsData[i].link,
+    }
+    console.log("foundSkills:" + foundSkills.length);
+    /*if(foundSkills.length<1){
+      delete jobsData[i].bodyText;
+      jobsData.splice(i,i+1);
+    }else{
+      jobsData[i].keyWords = foundSkills;
+      delete jobsData[i].bodyText;
+    }*/
+    jobJson.keyWords = foundSkills;
+    if(foundSkills.length>0){
+      filteredJobs.push(jobJson);
+    }
+    count++;
+  }
+  return filteredJobs;
+}
+
 app.post('/jobs', async (req,res) =>{
   console.log(req.body.option)
   console.log(req.body.location)
-  console.log("scrapping indeed");
-  const jobsData = await indeedScrapper.scrapeIndeed(req.body.option,req.body.location)
-  console.log("indeed scrapping finished");
-  console.log(jobsData);
+  console.log(req.body.skills)
+  const indeedData = await scrapeIndeed(req.body.option,req.body.location)
+  const indeedJobCards = await filterBySkills(indeedData,req.body.skills)
+  console.log('filtering complete')
+  console.log(indeedJobCards)
   res.send('success')
 })
 
